0d61ec9285b00a6dfea8330211c98ba7bfecb139
Stefano Casasola <ste.casasola@gmail.com>
2024-10-07 10:59:23 +0200

    adds our first JS lines

--- script/index.js
+++ script/index.js
@@ -1,1 +1,31 @@
+// la direttiva console.log() vi permette di STAMPARE un messaggio nella CONSOLE
+// del browser
 console.log('Hello World!')
+console.log(10 + 5)
+
+// il nostro scopo da sviluppatori è creare ALGORITMI
+// un ALGORITMO è una sequenza numerata di passaggi che portano alla risoluzione
+// di un problema; dato lo stesso problema, con lo stesso algoritmo è possibile
+// risolverlo infinite volte.
+
+// VARIABILI
+// una variabile è una "scatola", una "bacinella", un "contenitore" per un risultato
+// temporaneo. Dentro ad una variabile si inserisce un "valore".
+// Si chiama "variabile" perchè il suo contenuto (il suo valore) può cambiare nel
+// corso della sua vita.
+
+// le variabili in JS si definiscono con la parola chiave "let"
+let myFantasticDog // ho creato una variabile di nome "dog"
+// di solito in JS i nome delle variabili NON cominciano con numeri
+// in JS per le variabili si segue la nomenclatura "camelCase"
+// myDog, newElement, createMoreKittens
+myFantasticDog = 'Pluto' // ho assegnato alla variabile myFantasticDog il valore di "Pluto"
+// volendo potete condensare questi due passaggi in uno solo:
+let myAmazingCat = 'Tom' // la variabile viene GIÀ inizializzata con un contenuto
+
+let age = 25 // la variabile age contiene un NUMERO, il colore è diverso!
+
+// test = 'ok!' // ERRORE!
+// let test
+// ho commentato le due righe sopra perchè darebbero ERRORE!
+// PRIMA si DICHIARA una variabile (con let), POI la si utilizza
